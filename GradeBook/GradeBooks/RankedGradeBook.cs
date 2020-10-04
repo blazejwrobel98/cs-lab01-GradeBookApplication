5d5600a8185590369ca0791434634fe8af6e8e4e
Błażej Wróbel <blazej.wrobel@microsoft.wsei.edu.pl>
2020-10-04 12:55:51 +0200

    5 tests completed

---
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,28 @@
+﻿using GradeBook.Enums;
+
+namespace GradeBook.GradeBooks
+{
+    public class RankedGradeBook : BaseGradeBook
+    {
+        string Name;
+        GradeBookType Type = new GradeBookType();
+        public RankedGradeBook(string name) : base(name)
+        {
+            Name = name;
+            Type = GradeBookType.Ranked;
+        }
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (averageGrade >= 90)
+                return 'A';
+            else if (averageGrade >= 80)
+                return 'B';
+            else if (averageGrade >= 70)
+                return 'C';
+            else if (averageGrade >= 60)
+                return 'D';
+            else
+                return 'F';
+        }
+    }
+}
---
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -0,0 +1,16 @@
+﻿using GradeBook.Enums;
+
+namespace GradeBook.GradeBooks
+{
+    public class StandardGradeBook : BaseGradeBook
+    {
+        string Name;
+        GradeBookType Type = new GradeBookType();
+        public StandardGradeBook(string name) : base(name)
+        {
+            Name = name;
+            Type = GradeBookType.Standard;
+        }
+        
+    }
+}
